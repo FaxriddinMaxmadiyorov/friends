b093496cb819728789f3764958412bb092163710
Makhmadiyorov Fakhriddin <makhmadiyorovfakhriddin@gmail.com>
2023-09-05 14:55:57 +0500

    added chat function

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -8,7 +8,7 @@ class ChatRoomsController < ApplicationController
 
   # GET /chat_rooms/1 or /chat_rooms/1.json
   def show
-    @chat_rooms = ChatRoom.all
+    @chat_rooms  = ChatRoom.all
     render 'index'
   end
 
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,15 +1,18 @@
 import consumer from "./consumer"
 
 document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = Number(room_element.getAttribute('data-room-id'));
 
-  const element = document.getElementById('room-id');
-  const room_id = element.getAttribute('data-room-id');
+  console.log(consumer.subscriptions);
 
-  console.log(room_id);
+  consumer.subscriptions.subscriptions.forEach((subscriptions) => {
+    consumer.subscriptions.remove(subscription);
+  });
 
   consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id }, {
     connected() {
-      console.log('Connected to RoomChannel' + room_id);
+      console.log('Connected to RoomChannel ' + room_id);
       // Called when the subscription is ready for use on the server
     },
 
@@ -18,8 +21,22 @@ document.addEventListener('turbolinks:load', () => {
     },
 
     received(data) {
-      console.log(data);
+      console.log('data');
+
+      const user_element = document.getElementById('user-id');
+      const user_id = Number(user_element.getAttribute('data-user-id'));
+
+      let html;
+
+      if (user_id === data.message.user_id) {
+        html = data.mine;
+      } else {
+        html = data.theirs;
+      }
+
+      const messageContainer = document.getElementById('messages');
+      messageContainer.innerHTML = messageContainer.innerHTML + html;
       // Called when there's incoming data on the websocket for this channel
     }
   });
-})
+});
